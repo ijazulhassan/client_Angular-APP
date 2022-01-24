0bc3b603bb623cc34e2df435a92baf8b0bcbba99
Ijaz Ul Hassan <ijaz.h@pacsquare.com>
2022-01-24 14:31:21 +0500

    end of api binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Client angular-app ';
+  users: any;
+
+  constructor(private _http: HttpClient) { }
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers() {
+    this._http.get('https://localhost:5001/api/Users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
+
