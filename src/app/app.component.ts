b0caa8266b4bbcdf4157e848015522c712444de9
ijazulhassan <ijaz.ul.hassan@articares.com>
2022-12-08 20:48:49 +0500

    initial changes in environment and others

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const  baseurl = `${environment.apiUrl}App`;
 
 @Component({
   selector: 'app-root',
@@ -9,13 +12,19 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'Client angular-app ';
   users: any;
+  //const baseUrl = `${environment.apiUrl}/User`;
+ 
 
   constructor(private _http: HttpClient) { }
   ngOnInit() {
     this.getUsers();
   }
+
+
+
+
   getUsers() {
-    this._http.get('https://localhost:5001/api/Users').subscribe(response => {
+    this._http.get(`${baseurl}`).subscribe(response => {
       this.users = response;
     }, error => {
       console.log(error);
